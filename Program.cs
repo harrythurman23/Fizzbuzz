97b1601c220b81482a2706d87e865f80e88f4d2b
harrythurman23 <125393979+harrythurman23@users.noreply.github.com>
2023-04-28 18:08:44 -0400

    first and only commit hopefully
    still getting used to visual studio and github for desktop

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //demonstrating a for loop and the modulus operator
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
